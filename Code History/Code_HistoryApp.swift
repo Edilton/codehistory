eb5cf25416a00450ce5b82661a009c1c44af07d8
Edilton <ediltondanniken@gmail.com>
2021-03-16 16:07:59 -0300

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Edilton Danniken on 16/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Code History/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Code History
+//
+//  Created by Edilton Danniken on 16/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
